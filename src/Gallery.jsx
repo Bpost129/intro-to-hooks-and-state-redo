206ca430b0acb7b089672f66170bb07d5183db7d
Bpost129 <Bpost129@yahoo.com>
2024-11-27 01:19:33 -0500

    add state variable to hide or show sculpture description

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,12 +2,17 @@ import { useState } from "react"
 import { sculptureList } from "./data"
 
 const Gallery = () => {
-
   const [index, setIndex] = useState(0)
+  const [showMore, setShowMore] = useState(false)
+
   let sculpture = sculptureList[index]
 
   function handleClick() {
     setIndex(index + 1)
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore)
   }
 
   return (
@@ -22,11 +27,14 @@ const Gallery = () => {
       <h3>  
         ({index + 1} of {sculptureList.length})
       </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
       <img 
         src={sculpture.url} 
         alt={sculpture.alt}
       />
-      <p>{sculpture.description}</p>
     </div>
   )
 }
